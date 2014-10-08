25f45215ef0fed45ececdd32ff8ce9e9bc1c5fd0
Esa Virtanen <esa.virtanen@reaktor.fi>
2014-10-08 16:25:31 +0300

    add tweet button (and not-quite-working-commented-out fb button)

--- resources/public/js/index.js
+++ resources/public/js/index.js
@@ -1,7 +1,18 @@
 (function() {
   var getUrl, init, isValidUrl, errorHtml;
 
-  errorHtml = '<p class="error">Oops, couldn\'t load this image!</p>';
+  errorHtml = function() {
+      return '<p class="error">Oops, couldn\'t load this image!</p>';
+  }
+
+  tweetButtonHtml = function(url) {
+      return('<a href="https://twitter.com/share" class="twitter-share-button" data-url="'+url+'" data-text="I just took a selfie with Arnold. Take yours: http://arskafy.fi" data-related="ReaktorNow" data-hashtags="reaktorarska">Tweet</a>');
+  }
+
+//  fbButtonHtml = function(url) {
+//      return('<fb:share-button href="'+url+'">Share on Facebook</fb:share-button>');
+//      // return('<div class="fb-share-button" data-href="'+url+'">Share on Facebook</div>');
+//  }
 
   init = function() {
     var inputValid;
@@ -9,17 +20,20 @@
       return $('#submit-button').prop('disabled', !isValid);
     });
     $('#url-input').trigger('input');
-    return $('#submit-button').asEventStream('click').map(getUrl).onValue(function(url) {
-      var li, src, img;
-      src = "/doit?url=" + url;
-      img = $('<img>', {src: src}).error(function() {
-          $(this).closest('li').html(errorHtml);
-      })
-      li = $('<li>').append(img, $('<a>', {
-        href: src
-      }).text("Link"));
+    $('#submit-button').asEventStream('click').map(getUrl).onValue(function(url) {
+      var src = "/doit?url=" + url;
+      var img = $('<img>', {src: src}).error(function() {
+          $(this).closest('li').html(errorHtml());
+      });
+      var url = 'http://arskafy.fi' + src;
+      var tweetButton = $(tweetButtonHtml(url));
+      // var fbButton = $(fbButtonHtml(url));
+      var imageLink = $('<a>', {href: src}).text("Link");
+      li = $('<li>').append(img, imageLink, tweetButton);
       $('#url-input').val('');
       $('#images').prepend(li);
+      // FB.XFBML.parse();
+      twttr.widgets.load();
     });
   };
 
